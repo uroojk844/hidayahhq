a960d6c6f1e1b095956be188f7fc9b43f7e23ee2
Urooj Khan <khan.9386@apac.teleperformance.com>
2025-07-03 15:38:44 +0530

    first commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import { arText, enText, title } from '../store/style';
+</script>
+
+<template>
+    <aside>
+        <div class="title">HidayahHQ</div>
+        <details open>
+            <summary class="heading">Title</summary>
+            <form>
+                <div class="group">
+                    <label for="">Size <span>{{ title.size / 10 }}</span></label>
+                    <input type="range" min="1" max="100" v-model="title.size" />
+                </div>
+                <div class="dflex">
+                    <label for="">Color</label>
+                    <input type="color" v-model="title.color" />
+                </div>
+                <div class="dflex">
+                    <label for="">Background color</label>
+                    <input type="color" v-model="title.bg" />
+                </div>
+                <div class="group">
+                    <label for="">Width <span>{{ title.width }}</span></label>
+                    <input type="range" min="0" max="474" v-model="title.width" />
+                </div>
+                <div class="group">
+                    <label for="">Text alignment</label>
+                    <select v-model="title.textAligment">
+                        <option value="left">Left</option>
+                        <option value="center">Center</option>
+                        <option value="right">Right</option>
+                    </select>
+                </div>
+                <div class="group">
+                    <label for="">Alignment</label>
+                    <select v-model="title.selfAligment">
+                        <option value="start">Start</option>
+                        <option value="center">Center</option>
+                        <option value="end">End</option>
+                    </select>
+                </div>
+            </form>
+        </details>
+        <details>
+            <summary class="heading">Arabic text</summary>
+            <form>
+                <div class="group">
+                    <label for="">Size <span>{{ arText.size / 10 }}</span></label>
+                    <input type="range" min="1" max="100" v-model="arText.size" />
+                </div>
+                <div class="dflex">
+                    <label for="">Color</label>
+                    <input type="color" v-model="arText.color" />
+                </div>
+                <div class="dflex">
+                    <label for="">Background color</label>
+                    <input type="color" v-model="arText.bg" />
+                </div>
+                <div class="group">
+                    <label for="">Width <span>{{ arText.width }}</span></label>
+                    <input type="range" min="0" max="474" v-model="arText.width" />
+                </div>
+                <div class="group">
+                    <label for="">Text alignment</label>
+                    <select v-model="arText.textAligment">
+                        <option value="left">Left</option>
+                        <option value="center">Center</option>
+                        <option value="right">Right</option>
+                    </select>
+                </div>
+                <div class="group">
+                    <label for="">Alignment</label>
+                    <select v-model="arText.selfAligment">
+                        <option value="start">Start</option>
+                        <option value="center">Center</option>
+                        <option value="end">End</option>
+                    </select>
+                </div>
+            </form>
+        </details>
+        <details>
+            <summary class="heading">English text</summary>
+            <form>
+                <div class="group">
+                    <label for="">Size <span>{{ enText.size / 10 }}</span></label>
+                    <input type="range" min="1" max="100" v-model="enText.size" />
+                </div>
+                <div class="dflex">
+                    <label for="">Color</label>
+                    <input type="color" v-model="enText.color" />
+                </div>
+                <div class="dflex">
+                    <label for="">Background color</label>
+                    <input type="color" v-model="enText.bg" />
+                </div>
+                <div class="group">
+                    <label for="">Width <span>{{ enText.width }}</span></label>
+                    <input type="range" min="0" max="474" v-model="enText.width" />
+                </div>
+                <div class="group">
+                    <label for="">Text alignment</label>
+                    <select v-model="enText.textAligment">
+                        <option value="left">Left</option>
+                        <option value="center">Center</option>
+                        <option value="right">Right</option>
+                    </select>
+                </div>
+                <div class="group">
+                    <label for="">Alignment</label>
+                    <select v-model="enText.selfAligment">
+                        <option value="start">Start</option>
+                        <option value="center">Center</option>
+                        <option value="end">End</option>
+                    </select>
+                </div>
+            </form>
+        </details>
+    </aside>
+</template>
+
+<style scoped lang="css">
+
+aside{
+    display: grid;
+    align-content: start;
+    gap: 16px;
+    overflow-y: auto;
+    max-height: 100vh;
+}
+form {
+    padding: 16px;
+}
+
+input[type='range'] {
+    accent-color: gray;
+}
+
+label {
+    display: flex;
+    justify-content: space-between;
+}
+
+.dflex {
+    display: flex;
+    justify-content: space-between
+}
+
+.heading {
+    font-size: 18px;
+}
+</style>
---
+++ src/store/style.ts
@@ -0,0 +1,77 @@
+import { reactive, ref, watch } from "vue";
+
+export const titleRef = ref<HTMLElement>();
+export const arRef = ref<HTMLElement>();
+export const enRef = ref<HTMLElement>();
+
+export const title = reactive({
+  size: 12,
+  color: "#ffffff",
+  bg: "",
+  width: 0,
+  textAligment: 'center',
+  selfAligment: 'center',
+});
+
+export const arText = reactive({
+  size: 20,
+  color: "#ffffff",
+  bg: "",
+  width: 0,
+  textAligment: 'center',
+  selfAligment: 'center',
+});
+
+export const enText = reactive({
+  size: 14,
+  color: "#ffffff",
+  bg: "",
+  width: 0,
+  textAligment: 'center',
+  selfAligment: 'center',
+});
+
+window.onload = () => {
+  if (titleRef.value) {
+    title.width = titleRef.value.offsetWidth;
+  }
+  if (arRef.value) {
+    arText.width = arRef.value.offsetWidth;
+  }
+  if (enRef.value) {
+    enText.width = enRef.value.offsetWidth;
+  }
+};
+
+watch(title, () => {
+  if (!titleRef.value) return;
+
+  titleRef.value.style.fontSize = title.size / 10 + "em";
+  titleRef.value.style.color = title.color;
+  titleRef.value.style.backgroundColor = title.bg;
+  titleRef.value.style.width = title.width + "px";
+  titleRef.value.style.textAlign = title.textAligment;
+  titleRef.value.style.placeSelf = title.selfAligment;
+});
+
+watch(arText, () => {
+  if (!arRef.value) return;
+
+  arRef.value.style.fontSize = arText.size / 10 + "em";
+  arRef.value.style.color = arText.color;
+  arRef.value.style.backgroundColor = arText.bg;
+  arRef.value.style.width = arText.width + "px";
+  arRef.value.style.textAlign = arText.textAligment;
+  arRef.value.style.placeSelf = arText.selfAligment;
+});
+
+watch(enText, () => {
+  if (!enRef.value) return;
+
+  enRef.value.style.fontSize = enText.size / 10 + "em";
+  enRef.value.style.color = enText.color;
+  enRef.value.style.backgroundColor = enText.bg;
+  enRef.value.style.width = enText.width + "px";
+  enRef.value.style.textAlign = enText.textAligment;
+  enRef.value.style.placeSelf = enText.selfAligment;
+});
